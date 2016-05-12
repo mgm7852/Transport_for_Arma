e4f007083c69183f7a04b7a3719826fdf7a67e2e
mgm7852 <mgm7852@gmail.com>
2016-05-12 17:04:27 +0100

    Add: builder bash script
    (cherry picked from commit 8a06ab3419b7387a1da702eca59ccfb5788ff4ad)

---
+++ for_developers/release_engineering/___builder___BUILD_NOW.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#======================================##======================================#
+# *** BUILDER SCRIPT BREAK DOWN ***		i.e.: How does this work?
+#======================================##======================================#
+#		'/c/git_repos.private/priv__DEV_DOCS/Release_Engineering.PUB_[MOVE_THIS_TO_PUB_REPO]/BUILDER_SCRIPT_BREAK_DOWN.sh'
+#
+#
+#
+#
+#
+#
+#======================================##======================================#
+# *** CONFIGURATION SECTION ***
+#
+# PATH_WORKDIR_ROOT: We will be operating from within this directory (not that it matters here)
+PATH_WORKDIR_ROOT='/c/tmp'
+#
+# PATH_MODIFICATION_ROOT: This is the top level directory - we will modify anything under this
+PATH_MODIFICATION_ROOT='/c/git_repos.public/pub__Transport_for_Arma/source_code2/'
+#======================================##======================================#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#======================================##======================================#
+# *** MAIN ***
+#
+# TASK:	Find all files under source_code which contain the string:		//__builder___DELETE_THIS
+cd $PATH_WORKDIR_ROOT
+#
+#======================================#
+# TASK:	Find all files under source_code which contain the string:		//__builder___DELETE_THIS
+#		then modify these matching files in place with the in-line sed expression
+#			modification #1:	delete the whole line
+grep --recursive --files-with-matches --null "//__builder___DELETE_THIS" $PATH_MODIFICATION_ROOT | xargs -0 sed --in-place "/\/\/__builder___DELETE_THIS/d"
+#
+#======================================#
+# TASK:	Find all files under source_code which contain the string:		//__builder___UNCOMMENT_THIS
+#		then modify these matching files in place with the in-line sed expression
+#			modification #1:	delete the very first (most-left) two characters on each line
+#			modification #2:	remove the string //__builder___UNCOMMENT_THIS
+grep --recursive --files-with-matches --null "//__builder___UNCOMMENT_THIS" $PATH_MODIFICATION_ROOT | xargs -0 sed --in-place -e '/__builder___UNCOMMENT_THIS/{s/#//g;s/\/\/__builder___UNCOMMENT_THIS//g;s/\/\///g;}'
+#======================================##======================================#
+#
+#
+#
+#
+#
+#======================================#
+# *** LET THE DEV SEE ***
+#sleep 5s # Waits 5 seconds.
+read -p "Press ENTER to exit..."
+#======================================#
+#
+#EOF
